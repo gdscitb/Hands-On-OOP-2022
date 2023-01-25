0e69d92749a366f32b6584e6b049a315b483c462
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-01-25 12:06:00 +0700

    Adding all rules and declare who wins program

--- MohFairuzAlauddinYahya_ITB/main.cpp
+++ MohFairuzAlauddinYahya_ITB/main.cpp
@@ -3,16 +3,20 @@
 #include <random>
 #include <typeinfo>
 using namespace std;
+
+// ! INTERFACE
 class Skill
 {
 public:
     string name;
-    int damage;
+    double damage;
+    double manaCost;
 
-    Skill(string n, int d)
+    Skill(string n, double d, double mC)
     {
         name = n;
         damage = d;
+        manaCost = mC;
     }
 };
 
@@ -23,20 +27,22 @@ public:
     double damage;
     double health;
     double defense;
+    double mana;
     vector<Skill> skillList;
 
     Hero() {}
-    Hero(string n, double dmg, double w, double def)
+    Hero(string n, double dmg, double w, double def, double m)
     {
         name = n;
         damage = dmg;
         health = w;
         defense = def;
+        mana = m;
         // TODO : GENERATE BUAT PRINT INFO
     }
-    void addJurus(string name, int damage)
+    void addJurus(string name, double damage, double manaCost)
     {
-        skillList.push_back(Skill(name, damage));
+        skillList.push_back(Skill(name, damage, manaCost));
     }
 };
 
@@ -63,13 +69,15 @@ public:
     string name;
     double defense;
     double addHealth;
+    double addMana;
 
     Armor() {}
-    Armor(string n, double def, double aH)
+    Armor(string n, double def, double aH, double aM)
     {
         name = n;
         defense = def;
         addHealth = aH;
+        addMana = aM;
         // TODO : GENERATE BUAT PRINT INFO
     }
 };
@@ -92,19 +100,19 @@ public:
 };
 
 // ! INSTANCE CLASS
-Hero hero1("Saitama", 20, 95, 5);
-Hero hero2("Chid Kagenou", 14, 20, 7);
-Hero hero3("Yoriichi Tsugikuni", 18, 85, 4);
-Hero hero4("Loid Forger", 12, 96, 4);
+Hero hero1("Saitama", 20, 95, 10, 50);
+Hero hero2("Chid Kagenou", 14, 80, 15, 45);
+Hero hero3("Yoriichi Tsugikuni", 18, 85, 20, 35);
+Hero hero4("Loid Forger", 12, 96, 18, 40);
 
 Weapon sword("Sword", 10, 25);
 Weapon pistols("Pistols", 6, 30);
 Weapon arrow("Arrow", 5, 33);
 Weapon spear("Spear", 8, 23);
 
-Armor gold("Gold", 20, 10);
-Armor silver("Silver", 15, 20);
-Armor bronze("Bronze", 10, 30);
+Armor gold("Gold", 20, 10, 25);
+Armor silver("Silver", 15, 20, 30);
+Armor bronze("Bronze", 10, 30, 35);
 
 // ! GENERATING BOT HERO
 class Bot : public Hero, public Weapon, public Armor
@@ -148,31 +156,31 @@ public:
         {
         case 1:
             chosenWeapon = sword;
-            chosenHero.addJurus("Star Strike", 15);
-            chosenHero.addJurus("Super Air Strike", 20);
-            chosenHero.addJurus("Sword Stab", 25);
-            chosenHero.addJurus("X strike", 30);
+            chosenHero.addJurus("Star Strike", 15, 10);
+            chosenHero.addJurus("Super Air Strike", 20, 18);
+            chosenHero.addJurus("Sword Stab", 25, 23);
+            chosenHero.addJurus("X strike", 30, 30);
             break;
         case 2:
             chosenWeapon = pistols;
-            chosenHero.addJurus("Crazy Shot", 15);
-            chosenHero.addJurus("Destroy Shot", 20);
-            chosenHero.addJurus("Crazy Gun", 25);
-            chosenHero.addJurus("Focus Shot", 30);
+            chosenHero.addJurus("Crazy Shot", 15, 10);
+            chosenHero.addJurus("Destroy Shot", 20, 18);
+            chosenHero.addJurus("Crazy Gun", 25, 23);
+            chosenHero.addJurus("Focus Shot", 30, 30);
             break;
         case 3:
             chosenWeapon = arrow;
-            chosenHero.addJurus("Falling Star", 15);
-            chosenHero.addJurus("Shied Destroy", 20);
-            chosenHero.addJurus("Concentration", 25);
-            chosenHero.addJurus("Triangle of Light", 30);
+            chosenHero.addJurus("Falling Star", 15, 10);
+            chosenHero.addJurus("Shied Destroy", 20, 18);
+            chosenHero.addJurus("Concentration", 25, 23);
+            chosenHero.addJurus("Triangle of Light", 30, 30);
             break;
         case 4:
             chosenWeapon = spear;
-            chosenHero.addJurus("Deadly Thrust", 15);
-            chosenHero.addJurus("Rotate Spear", 20);
-            chosenHero.addJurus("Breaking Limit", 25);
-            chosenHero.addJurus("Twin Brother", 30);
+            chosenHero.addJurus("Deadly Thrust", 15, 10);
+            chosenHero.addJurus("Rotate Spear", 20, 18);
+            chosenHero.addJurus("Breaking Limit", 25, 23);
+            chosenHero.addJurus("Twin Brother", 30, 30);
             break;
         }
 
@@ -195,9 +203,11 @@ public:
 
     void initialInfo()
     {
-        cout << "Bot :\nHero \t: " << chosenHero.name << " ( Damage:" << chosenHero.damage << ", Health:" << chosenHero.health << ", Defense:" << chosenHero.defense << "% )" << endl;
+        cout << "Bot :\nHero \t: " << chosenHero.name << " ( Damage:" << chosenHero.damage << ", Health:" << chosenHero.health << ", Defense:" << chosenHero.defense << "%, "
+             << "Mana: " << chosenHero.mana << " )" << endl;
         cout << "Weapon : " << chosenWeapon.name << " ( Additional Damage: " << chosenWeapon.damage << ", Critical: " << chosenWeapon.critical << "% )" << endl;
-        cout << "Armor : " << chosenArmor.name << "( Addition Health: " << chosenArmor.addHealth << ", Defense: " << chosenArmor.defense << "% )" << endl;
+        cout << "Armor : " << chosenArmor.name << "( Addition Health: " << chosenArmor.addHealth << ", Defense: " << chosenArmor.defense << "%, "
+             << "Mana: " << chosenArmor.addMana << " )" << endl;
     }
 
     void resultInfo()
@@ -206,10 +216,12 @@ public:
         chosenHero.damage += (chosenWeapon.critical / 100) * chosenHero.damage;
         chosenHero.health += chosenArmor.addHealth;
         chosenHero.defense += chosenArmor.defense;
+        chosenHero.mana += chosenArmor.addMana;
         ;
-        cout << "Total Attack: " << chosenHero.damage << endl;
+        cout << "Total Damage: " << chosenHero.damage << endl;
         cout << "Total Health: " << chosenHero.health << endl;
         cout << "Total Defense: " << chosenHero.defense << "%" << endl;
+        cout << "Total Mana: " << chosenHero.mana << endl;
     }
 };
 
@@ -245,7 +257,7 @@ public:
     string getWinner()
     {
         botChoice = generateBotChoice();
-        
+
         if (userChoice == "rock")
         {
             if (botChoice == "paper")
@@ -290,10 +302,10 @@ int main()
 
     // ! PLAYER CHOOSE HERO
     cout << "\nPilih hero yang ingin digunakan:" << endl;
-    cout << "1. " << hero1.name << "\t\t( Damage : " << hero1.damage << ", Health : " << hero1.health << ", Defense : " << hero1.defense << "% )" << endl;
-    cout << "2. " << hero2.name << "\t\t( Damage : " << hero2.damage << ", Health : " << hero2.health << ", Defense : " << hero2.defense << "% )" << endl;
-    cout << "3. " << hero3.name << "\t( Damage : " << hero3.damage << ", Health : " << hero3.health << ", Defense : " << hero3.defense << "% )" << endl;
-    cout << "4. " << hero4.name << "\t\t( Damage : " << hero4.damage << ", Health : " << hero4.health << ", Defense : " << hero4.defense << "% )" << endl;
+    cout << "1. " << hero1.name << "\t\t( Damage : " << hero1.damage << ", Health : " << hero1.health << ", Defense : " << hero1.defense << "%, Mana: " << hero1.mana << " )" << endl;
+    cout << "2. " << hero2.name << "\t\t( Damage : " << hero2.damage << ", Health : " << hero2.health << ", Defense : " << hero2.defense << "%, Mana: " << hero2.mana << " )" << endl;
+    cout << "3. " << hero3.name << "\t( Damage : " << hero3.damage << ", Health : " << hero3.health << ", Defense : " << hero3.defense << "%, Mana: " << hero3.mana << " )" << endl;
+    cout << "4. " << hero4.name << "\t\t( Damage : " << hero4.damage << ", Health : " << hero4.health << ", Defense : " << hero4.defense << "%, Mana: " << hero4.mana << " )" << endl;
 
     int choice;
     cin >> choice;
@@ -346,31 +358,31 @@ int main()
     {
     case 1:
         chosenWeapon = sword;
-        chosenHero.addJurus("Star Strike", 15);
-        chosenHero.addJurus("Super Air Strike", 20);
-        chosenHero.addJurus("Sword Stab", 25);
-        chosenHero.addJurus("X strike", 30);
+        chosenHero.addJurus("Star Strike", 15, 10);
+        chosenHero.addJurus("Super Air Strike", 20, 18);
+        chosenHero.addJurus("Sword Stab", 25, 23);
+        chosenHero.addJurus("X strike", 30, 30);
         break;
     case 2:
         chosenWeapon = pistols;
-        chosenHero.addJurus("Crazy Shot", 15);
-        chosenHero.addJurus("Destroy Shot", 20);
-        chosenHero.addJurus("Crazy Gun", 25);
-        chosenHero.addJurus("Focus Shot", 30);
+        chosenHero.addJurus("Crazy Shot", 15, 10);
+        chosenHero.addJurus("Destroy Shot", 20, 18);
+        chosenHero.addJurus("Crazy Gun", 25, 23);
+        chosenHero.addJurus("Focus Shot", 30, 30);
         break;
     case 3:
         chosenWeapon = arrow;
-        chosenHero.addJurus("Falling Star", 15);
-        chosenHero.addJurus("Shied Destroy", 20);
-        chosenHero.addJurus("Concentration", 25);
-        chosenHero.addJurus("Triangle of Light", 30);
+        chosenHero.addJurus("Falling Star", 15, 10);
+        chosenHero.addJurus("Shied Destroy", 20, 18);
+        chosenHero.addJurus("Concentration", 25, 23);
+        chosenHero.addJurus("Triangle of Light", 30, 30);
         break;
     case 4:
         chosenWeapon = spear;
-        chosenHero.addJurus("Deadly Thrust", 15);
-        chosenHero.addJurus("Rotate Spear", 20);
-        chosenHero.addJurus("Breaking Limit", 25);
-        chosenHero.addJurus("Twin Brother", 30);
+        chosenHero.addJurus("Deadly Thrust", 15, 10);
+        chosenHero.addJurus("Rotate Spear", 20, 18);
+        chosenHero.addJurus("Breaking Limit", 25, 23);
+        chosenHero.addJurus("Twin Brother", 30, 30);
         break;
     }
 
@@ -381,9 +393,9 @@ int main()
 
     // ! PLAYER CHOOSE ARMOR
     cout << "\nPilih armor yang ingin digunakan:" << endl;
-    cout << "1. " << gold.name << "\t\t( Additional Health : " << gold.addHealth << ", Defense : " << gold.defense << "% )" << endl;
-    cout << "2. " << silver.name << "\t( Additional Health : " << silver.addHealth << ", Defense : " << silver.defense << "% )" << endl;
-    cout << "3. " << bronze.name << "\t( Additional Health : " << bronze.addHealth << ", Defense : " << bronze.defense << "% )" << endl;
+    cout << "1. " << gold.name << "\t\t( Additional Health : " << gold.addHealth << ", Defense : " << gold.defense << "%, Additional Mana: " << gold.addMana << " )" << endl;
+    cout << "2. " << silver.name << "\t( Additional Health : " << silver.addHealth << ", Defense : " << silver.defense << "%, Additional Mana: " << silver.addMana << " )" << endl;
+    cout << "3. " << bronze.name << "\t( Additional Health : " << bronze.addHealth << ", Defense : " << bronze.defense << "%, Additional Mana: " << bronze.addMana << " )" << endl;
 
     int armorChoice;
     cin >> armorChoice;
@@ -411,6 +423,8 @@ int main()
     cout << "Anda telah memilih " << chosenHero.name << " dengan senjata " << chosenWeapon.name << ", dan armor " << chosenArmor.name << endl;
     chosenHero.health += chosenArmor.addHealth;
     chosenHero.defense += chosenArmor.defense;
+    chosenHero.mana += chosenArmor.addMana;
+    // * WHAT USER GET WILL BE INSTANCE ON PLAYER
     Player player(chosenHero, chosenWeapon, chosenArmor);
 
     // ! POP UP RESULT PLAYER'S HERO INFO
@@ -420,57 +434,142 @@ int main()
     cout << "Total Attack: " << player.chosenHero.damage << endl;
     cout << "Total Health: " << player.chosenHero.health << endl;
     cout << "Total Defense: " << player.chosenHero.defense << "% " << endl;
+    cout << "Total Mana: " << player.chosenHero.mana << endl;
 
     // ! USING ROCK PAPER SCISSORS TO DETERMINE WHO'S PLAYS
-
-     GamePlay bkg(player.chosenHero, bot.chosenHero);
+    GamePlay bkg(player.chosenHero, bot.chosenHero);
 
     while (player.chosenHero.health > 0 && bot.chosenHero.health > 0)
     {
-          cout << "\nPilih Giliran:" << endl;
-    cout << "1. Rock " << endl;  
-    cout << "2. Paper" << endl; 
-    cout << "3. Scissors" << endl;
-    int turnPlayer;
-    cin >> turnPlayer;
-    while (cin.fail() || turnPlayer < 1 || turnPlayer > 3)
-    {
-        cout << "Pilihan tidak valid. Silakan pilih kembali:" << endl;
-        cin.clear();                                         // untuk membersihkan error flag
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // untuk menghapus input yang tidak valid
-        cin >> turnPlayer;
-    }
 
-    switch (turnPlayer)
-    {
-    case 1:
-        bkg.userChoice = "rock";
-        break;
-    case 2:
-        bkg.userChoice = "paper";
-        break;
-    case 3:
-        bkg.userChoice = "scissors";
-        break;
-    }
+        cout << "\nPilih Giliran:" << endl;
+        cout << "1. Rock " << endl;
+        cout << "2. Paper" << endl;
+        cout << "3. Scissors" << endl;
+        int turnPlayer;
+        cin >> turnPlayer;
+        while (cin.fail() || turnPlayer < 1 || turnPlayer > 3)
+        {
+            cout << "Pilihan tidak valid. Silakan pilih kembali:" << endl;
+            cin.clear();                                         // untuk membersihkan error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // untuk menghapus input yang tidak valid
+            cin >> turnPlayer;
+        }
+
+        switch (turnPlayer)
+        {
+        case 1:
+            bkg.userChoice = "rock";
+            break;
+        case 2:
+            bkg.userChoice = "paper";
+            break;
+        case 3:
+            bkg.userChoice = "scissors";
+            break;
+        }
 
         string winner = bkg.getWinner();
 
         if (winner == "user")
         {
-            bot.chosenHero.health -= player.chosenHero.damage*(bot.chosenHero.defense/100);
-            cout << "Anda menang! Health Anda: " << player.chosenHero.health << endl;
-            cout << "Anda menang! Health Musuh: " << bot.chosenHero.health << endl;
+            double temporaryDamagePlayer;
+            int choice;
+            temporaryDamagePlayer = player.chosenHero.damage;
+            vector<int> skillIndex;
+
+            while (true)
+            {
+                for (int i = 0; i < player.chosenHero.skillList.size(); i++)
+                {
+                    if (player.chosenHero.skillList[i].manaCost <= player.chosenHero.mana)
+                    {
+                        skillIndex.push_back(i);
+                    }
+                }
+                if (skillIndex.size())
+                {
+                    cout << "Pilih jurus yang ingin digunakan:" << endl;
+                    for (int i = 0; i < skillIndex.size(); i++)
+                    {
+                        cout << i + 1 << ". " << player.chosenHero.skillList[skillIndex[i]].name << "(Additional Damage : " << player.chosenHero.skillList[skillIndex[i]].damage << ", Mana Cost: " << player.chosenHero.skillList[skillIndex[i]].manaCost << ")" << endl;
+                    }
+                    cin >> choice;
+                    while (cin.fail() || choice < 1 || choice > skillIndex.size())
+                    {
+                        cout << "Pilihan tidak valid. Silakan pilih kembali:" << endl;
+                        cin.clear();                                         // untuk membersihkan error flag
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // untuk menghapus input yang tidak valid
+                        cin >> choice;
+                    }
+                    if (player.chosenHero.mana >= player.chosenHero.skillList[choice - 1].manaCost)
+                    {
+                        temporaryDamagePlayer += player.chosenHero.skillList[choice - 1].damage;
+                        player.chosenHero.mana -= player.chosenHero.skillList[choice - 1].manaCost;
+                        bot.chosenHero.health -= temporaryDamagePlayer * (bot.chosenHero.defense / 100);
+                        bot.chosenHero.mana += 15;
+                        if (bot.chosenHero.health <= 0)
+                        {
+
+                            cout << "Health Musuh habis" << endl;
+                        }
+                        cout << "Anda menang! Health Anda: " << player.chosenHero.health << endl;
+                        cout << "Anda menang! Mana Anda: " << player.chosenHero.mana << endl;
+                        break;
+                    }
+                }
+                else
+                {
+                    cout << "Mana Habis" << endl;
+                    break;
+                }
+            }
         }
         else if (winner == "bot")
         {
-            player.chosenHero.health -= bot.chosenHero.damage*(player.chosenHero.defense/100);
-            cout << "Anda kalah! Health Anda: " << player.chosenHero.health << endl;
-            cout << "Anda kalah! Health Musuh: " << bot.chosenHero.health << endl;
+            double temporaryDamageBot;
+            int temporarySkillBot;
+            int choice;
+            temporaryDamageBot = bot.chosenHero.damage;
+            vector<int> skillIndex;
+
+            for (int i = 0; i < bot.chosenHero.skillList.size(); i++)
+            {
+                if (bot.chosenHero.skillList[i].manaCost <= bot.chosenHero.mana)
+                {
+                    skillIndex.push_back(i);
+                }
+            }
+            if (skillIndex.size())
+            {
+                mt19937 rng(random_device{}());
+                uniform_int_distribution<> dist(0, skillIndex.size() - 1);
+                int randomIndex = dist(rng);
+                choice = skillIndex[randomIndex];
+
+                if (bot.chosenHero.mana >= bot.chosenHero.skillList[choice - 1].manaCost)
+                {
+                    cout << "Bot Menyerang menggunakan Skill : " << bot.chosenHero.skillList[choice - 1].name << "( Damage: "<< bot.chosenHero.skillList[choice - 1].damage<<", Mana Cost : "<< bot.chosenHero.skillList[choice - 1].manaCost<<" )"<<endl;
+                    temporaryDamageBot += bot.chosenHero.skillList[choice - 1].damage;
+                    bot.chosenHero.mana -= bot.chosenHero.skillList[choice - 1].manaCost;
+                    player.chosenHero.health -= temporaryDamageBot * (bot.chosenHero.defense / 100);
+                    player.chosenHero.mana += 15;
+                    if (player.chosenHero.health <= 0)
+                    {
+
+                        cout << "Health Anda habis" << endl;
+                    }
+                    cout << "Anda kalah! Mana Anda: " << player.chosenHero.mana << endl;
+                    cout << "Anda kalah! Mana Musuh: " << bot.chosenHero.mana << endl;
+                    cout << "Anda kalah! Health Musuh: " << bot.chosenHero.health << endl;
+                }
+            }
         }
         else if (winner == "draw")
         {
             cout << "Hasil seri!" << endl;
+            player.chosenHero.mana += 10;
+            bot.chosenHero.mana += 10;
         }
         else
         {
@@ -478,11 +577,11 @@ int main()
         }
     }
 
-    if (hero1.health <= 0)
+    if (player.chosenHero.health <= 0)
     {
         cout << "Hero Anda kalah! Game over." << endl;
     }
-    else if (hero2.health <= 0)
+    else if (bot.chosenHero.health <= 0)
     {
         cout << "Hero Anda menang! Selamat!" << endl;
     }
